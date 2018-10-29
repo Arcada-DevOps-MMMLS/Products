92e138401b5db845dab124ea3fbd7d67e4dba0b5
LukasLonnroth <lukas.lonnroth@hotmail.se>
2018-10-29 16:05:49 +0200

    typo

--- test/api-test.js
+++ test/api-test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test ', function() {
-      assert.true(true);
+      assert.ok(true);
     });
   });
 });
