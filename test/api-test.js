03982916e5db03a2fa4a5a6778c4c4a134195136
LukasLonnroth <lukas.lonnroth@hotmail.se>
2018-10-29 16:03:18 +0200

    add mocha globally

---
+++ test/api-test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test ', function() {
+      assert.true(true);
+    });
+  });
+});
