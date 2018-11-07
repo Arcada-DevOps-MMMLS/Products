fbe3b6414a89b65798a70c58c49f38568c35c931
LukasLonnroth <lukas.lonnroth@hotmail.se>
2018-11-07 14:20:22 +0200

    products test

--- test/api-test.js
+++ test/api-test.js
@@ -1,8 +1,11 @@
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test Travis test ', function() {
-      assert.ok(true);
+    it('Getting products from /api/products', function() {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open( 'GET', 'https://productsdb-devops-arcada-2018.herokuapp.com/api/products', false );
+      xmlHttp.send( null );
+      assert.ok(xmlHttp.responseText != null);
     });
   });
 });
