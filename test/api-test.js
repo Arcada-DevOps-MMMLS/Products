a8edf3cb50a6361868b4338c1ecd71297312e77c
LukasLonnroth <lukas.lonnroth@hotmail.se>
2018-11-07 14:52:45 +0200

    add test for unauthenticated user

--- test/api-test.js
+++ test/api-test.js
@@ -2,25 +2,21 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var btoa = require('btoa');
 
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
+describe('API', function() {
+  describe('Tests for get and post', function() {
+
+    //Tests
     it('GET products from /api/products with status 200', function() {
       var xmlHttp = new XMLHttpRequest();
       xmlHttp.open( 'GET', 'https://productsdb-devops-arcada-2018.herokuapp.com/api/products', false );
       xmlHttp.send( null );
       assert.ok(xmlHttp.status === 200);
     });
-  });
-});
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('POST product to /api/products/new with status 200', function() {
-      var xmlHttp = new XMLHttpRequest();
-      xmlHttp.open( 'GET', 'https://productsdb-devops-arcada-2018.herokuapp.com/api/products/new', false );
-      xmlHttp.setRequestHeader("Authorization", "Basic " + btoa("admin:admin"));
+    it('Check that unauthorized user cannot access POST', function() {
+      xmlHttp.open( 'POST', 'https://productsdb-devops-arcada-2018.herokuapp.com/api/products/new', false );
       xmlHttp.send( null );
-      assert.ok(xmlHttp.status === 200);
+      assert.ok(xmlHttp.status === 401);
     });
   });
 });
