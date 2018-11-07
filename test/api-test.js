c7500b087c2bb998307de41e198f8021e6ed5342
LukasLonnroth <lukas.lonnroth@hotmail.se>
2018-11-07 14:43:11 +0200

    update api test

--- test/api-test.js
+++ test/api-test.js
@@ -3,8 +3,20 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
-    it('Getting products from /api/products', function() {
+    it('GET products from /api/products with status 200', function() {
       var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open( 'GET', 'https://productsdb-devops-arcada-2018.herokuapp.com/api/products', false );
+      xmlHttp.send( null );
+      assert.ok(xmlHttp.status === 200);
+    });
+  });
+});
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('POST products from /api/products with status 200', function() {
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.setRequestHeader("Authorization", "Basic " + btoa("admin:admin"));
       xmlHttp.open( 'GET', 'https://productsdb-devops-arcada-2018.herokuapp.com/api/products', false );
       xmlHttp.send( null );
       assert.ok(xmlHttp.status === 200);
